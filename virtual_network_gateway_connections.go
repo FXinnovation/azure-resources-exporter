eebe863ae3cb5b4c941f3209e5609601d6ba9f4d
Louis-Félix Tessier <louisfelix@gmail.com>
2019-11-28 17:51:19 -0500

    Adding virtual_network_gateway_connections

---
+++ virtual_network_gateway_connections.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-09-01/network"
+	"github.com/prometheus/common/log"
+)
+
+// VirtualNetworkGatewayConnectionsClient is the client implementation to VirtualNetworkGatewayConnections API
+type VirtualNetworkGatewayConnectionsClient struct {
+	Session      *AzureSession
+	Client       *network.VirtualNetworkGatewayConnectionsClient
+	Resources    Resources
+	ResourceType string
+}
+
+// VirtualNetworkGatewayConnections client interface
+type VirtualNetworkGatewayConnections interface {
+	GetVirtualNetworkGatewayConnections() (*[]network.VirtualNetworkGatewayConnection, error)
+	GetSubscriptionID() string
+}
+
+// NewVirtualNetworkGatewayConnections returns a new VirtualNetworkGatewayConnections client
+func NewVirtualNetworkGatewayConnections(session *AzureSession) VirtualNetworkGatewayConnections {
+
+	client := network.NewVirtualNetworkGatewayConnectionsClient(session.SubscriptionID)
+	client.Authorizer = session.Authorizer
+	resources := NewResources(session)
+
+	return &VirtualNetworkGatewayConnectionsClient{
+		Session:      session,
+		Client:       &client,
+		Resources:    resources,
+		ResourceType: "Microsoft.Network/connections",
+	}
+}
+
+// GetSubscriptionID return the client's Subscription ID
+func (client *VirtualNetworkGatewayConnectionsClient) GetSubscriptionID() string {
+	return client.Session.SubscriptionID
+}
+
+// GetVirtualNetworkGatewayConnections fetch VirtualNetworkGatewayConnections with connection status
+func (client *VirtualNetworkGatewayConnectionsClient) GetVirtualNetworkGatewayConnections() (*[]network.VirtualNetworkGatewayConnection, error) {
+	var connectionList []network.VirtualNetworkGatewayConnection
+
+	ressources, err := client.Resources.GetResources(client.ResourceType)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ressource := range *ressources {
+		labels, err := ParseResourceLabels(*ressource.ID)
+		if err != nil {
+			log.Errorf("Skipping virtual machine: %s", err)
+			continue
+		}
+
+		connection, err := client.Client.Get(context.Background(), labels["resource_group"], *ressource.Name)
+		if err != nil {
+			return nil, err
+		}
+
+		connectionList = append(connectionList, connection)
+	}
+
+	return &connectionList, nil
+}
