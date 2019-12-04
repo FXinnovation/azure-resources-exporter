f64295806f1e9eccc01123dbfac7b9e1a370e2ce
Louis-Félix Tessier <louisfelix@gmail.com>
2019-12-04 12:07:45 -0500

    Adding support for web_apps

---
+++ web_apps_collector.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"github.com/Azure/azure-sdk-for-go/services/web/mgmt/2019-08-01/web"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+)
+
+// AppUpState is the state of an App that is UP
+const AppUpState = "Running"
+
+// WebAppsCollector collect WebApps metrics
+type WebAppsCollector struct {
+	webApps WebApps
+}
+
+// NewWebAppsCollector returns the collector
+func NewWebAppsCollector(session *AzureSession) *WebAppsCollector {
+	webApps := NewWebApps(session)
+
+	return &WebAppsCollector{
+		webApps: webApps,
+	}
+}
+
+// Describe to satisfy the collector interface.
+func (c *WebAppsCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- prometheus.NewDesc("WebAppsCollector", "dummy", nil, nil)
+}
+
+// Collect metrics from Web Apps API
+func (c *WebAppsCollector) Collect(ch chan<- prometheus.Metric) {
+
+	appList, err := c.webApps.GetWebApps()
+	if err != nil {
+		log.Errorf("Failed to get web app list: %v", err)
+		ch <- prometheus.NewInvalidMetric(azureErrorDesc, err)
+		return
+	}
+
+	c.CollectAppUp(ch, appList)
+}
+
+// CollectAppUp converts WebApps state as a metric
+func (c *WebAppsCollector) CollectAppUp(ch chan<- prometheus.Metric, appList *[]web.Site) {
+
+	for _, app := range *appList {
+		up := 0.0
+		if *app.SiteProperties.State == AppUpState {
+			up = 1
+		}
+
+		labels, err := ParseResourceLabels(*app.ID)
+		if err != nil {
+			log.Errorf("Skipping WebApp: %s", err)
+			continue
+		}
+
+		labels["subscription_id"] = c.webApps.GetSubscriptionID()
+
+		ch <- prometheus.MustNewConstMetric(
+			prometheus.NewDesc("web_app_up", "Status of the web app", nil, labels),
+			prometheus.GaugeValue,
+			up,
+		)
+	}
+}
---
+++ web_apps_collector_test.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/services/web/mgmt/2019-08-01/web"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockedWebApps struct {
+	mock.Mock
+}
+
+func (mock *MockedWebApps) GetWebApps() (*[]web.Site, error) {
+	args := mock.Called()
+	return args.Get(0).(*[]web.Site), args.Error(1)
+}
+
+func (mock *MockedWebApps) GetSubscriptionID() string {
+	args := mock.Called()
+	return args.Get(0).(string)
+}
+
+func TestNewWebAppsCollector_OK(t *testing.T) {
+	want := "subscriptionID"
+	session, err := NewAzureSession(want)
+	if err != nil {
+		t.Errorf("Error occured %s", err)
+	}
+	_ = NewWebAppsCollector(session)
+
+	if err != nil {
+		t.Errorf("Error occured %s", err)
+	}
+}
+
+func TestCollectApp_Error(t *testing.T) {
+	webApps := MockedWebApps{}
+	collector := WebAppsCollector{
+		webApps: &webApps,
+	}
+	prometheus.MustRegister(&collector)
+
+	var appList []web.Site
+	webApps.On("GetWebApps").Return(&appList, errors.New("Unit test Error"))
+	webApps.On("GetSubscriptionID").Return("my_subscription")
+
+	req := httptest.NewRequest("GET", "/webhook", nil)
+	rr := httptest.NewRecorder()
+	handler := promhttp.Handler()
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("Wrong status code: got %v, want %v", status, http.StatusInternalServerError)
+	}
+}
+
+func TestCollectApp_Up(t *testing.T) {
+	webApps := MockedWebApps{}
+	collector := WebAppsCollector{
+		webApps: &webApps,
+	}
+
+	var appList []web.Site
+	id := "/subscriptions/my_subscription/resourceGroups/my_rg/providers/Microsoft.Web/site/my_app"
+	state := "Running"
+
+	appList = append(appList, web.Site{
+		SiteProperties: &web.SiteProperties{
+			State: &state,
+		},
+		ID: &id,
+	})
+
+	webApps.On("GetWebApps").Return(&appList, nil)
+	webApps.On("GetSubscriptionID").Return("my_subscription")
+
+	req := httptest.NewRequest("GET", "/webhook", nil)
+	rr := httptest.NewRecorder()
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(&collector)
+	handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Wrong status code: got %v, want %v", status, http.StatusOK)
+	}
+
+	want := `# HELP web_app_up Status of the web app
+# TYPE web_app_up gauge
+web_app_up{resource_group="my_rg",resource_name="my_app",subscription_id="my_subscription"} 1
+`
+	if rr.Body.String() != want {
+		t.Errorf("Unexpected body: got %v, want %v", rr.Body.String(), want)
+	}
+}
